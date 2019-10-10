bc3ffbf169d0a4b825fed5c25a869e15a3aface4
fiudurama <fiudurama@gmail.com>
2019-10-10 13:57:14 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let words = expr.split('**********'),
+        bit = '',
+        encodedBit = '',
+        output = '',
+        start = 0, end = 2;
+
+    words.forEach(function(word, index) {
+
+        let len = Math.floor(word.length / 2);
+
+        for (let i = 0; i < len; i++) {
+
+            bit = word.slice(start, end);
+
+            if ( bit == 10 ) {
+                encodedBit += '.'
+            }
+
+            if ( bit == 11 ) {
+                encodedBit += '-'
+            }
+
+            if ( end % 10 == 0 ) {
+                output += MORSE_TABLE[encodedBit];
+                encodedBit = '';
+            }
+
+            start = end;
+            end += 2;
+        };
+
+        if ( index != words.length - 1 ) {
+            output += ' ';
+        }
+
+        start = 0;
+        end = 2;
+    });
+
+    return output;
 }
 
 module.exports = {
